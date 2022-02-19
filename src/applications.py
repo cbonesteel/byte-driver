bcb24e2e69891aad70c51bf5fa8388dee66c7cd5
cbonesteel <cambones12@gmail.com>
2022-02-19 13:39:12 -0500

    fixed indent issue

--- src/applications.py
+++ src/applications.py
@@ -22,23 +22,23 @@ def main():
         delta = clock.tick(60)
 
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-                    elif(event.key == pygame.K_LEFT):
-                        car1.angle_change = -3
-                    elif event.key == pygame.K_RIGHT:
-                        car1.angle_change = 3
-                elif event.type == pygame.KEYUP:
-                    # Stop rotating if the player releases the keys.
-                    if event.key == pygame.K_RIGHT and car1.angle_change > 0:
-                        car1.angle_change = 0
-                    elif event.key == pygame.K_LEFT and car1.angle_change < 0:
-                        car1.angle_change = 0
+                elif(event.key == pygame.K_LEFT):
+                    car1.angle_change = -3
+                elif event.key == pygame.K_RIGHT:
+                    car1.angle_change = 3
+            elif event.type == pygame.KEYUP:
+                # Stop rotating if the player releases the keys.
+                if event.key == pygame.K_RIGHT and car1.angle_change > 0:
+                    car1.angle_change = 0
+                elif event.key == pygame.K_LEFT and car1.angle_change < 0:
+                    car1.angle_change = 0
 
         screen.fill((0,0,0))
         screen.blit(background, (0,0))
