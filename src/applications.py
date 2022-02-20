1b86c4593a9c833177f380ef66737304192a7588
andres <mudkip461@gmail.com>
2022-02-19 23:10:18 -0500

    updated Bitmap so that it uses the gameObj

--- src/applications.py
+++ src/applications.py
@@ -11,7 +11,7 @@ def main():
     clock = pygame.time.Clock()
 
     #background
-    bitmap = BitMap(6,2)
+    bitmap = BitMap(3,5)
     background = bitmap.getfinalimage()
     #background = pygame.image.load("./src/imgs/background.png")
 
